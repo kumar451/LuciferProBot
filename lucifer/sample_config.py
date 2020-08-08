7fb997e65f023aaee1e58041a8303312246df668
kumar451 <66391155+kumar451@users.noreply.github.com>
2020-08-08 14:53:30 +0530

    Update

--- lucifer/sample_config.py
+++ lucifer/sample_config.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def get_user_list(config, key):
-    with open('{}/lucifer_prorobot/{}'.format(os.getcwd(), config), 'r') as json_file:
+    with open('{}/lucifer/{}'.format(os.getcwd(), config), 'r') as json_file:
         return json.load(json_file)[key]
 
 
